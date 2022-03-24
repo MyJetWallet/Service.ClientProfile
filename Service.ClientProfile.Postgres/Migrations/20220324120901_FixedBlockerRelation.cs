e838bb6b8ff20854a9a3329805ca0acebd221c96
Hays R <wonderhays@gmail.com>
2022-03-24 14:09:15 +0200

    fixed blockers

---
+++ Service.ClientProfile.Postgres/Migrations/20220324120901_FixedBlockerRelation.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Service.ClientProfile.Postgres.Migrations
+{
+    public partial class FixedBlockerRelation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
