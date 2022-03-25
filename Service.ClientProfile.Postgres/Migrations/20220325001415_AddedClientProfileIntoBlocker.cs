6f46fe34f6180b93626f6b79ce10a42f507042d9
Hays R <wonderhays@gmail.com>
2022-03-25 02:14:38 +0200

    try to fix blockers ref

---
+++ Service.ClientProfile.Postgres/Migrations/20220325001415_AddedClientProfileIntoBlocker.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Service.ClientProfile.Postgres.Migrations
+{
+    public partial class AddedClientProfileIntoBlocker : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_blockers_profiles_ClientProfileClientId",
+                schema: "clientprofiles",
+                table: "blockers");
+
+            migrationBuilder.RenameColumn(
+                name: "ClientProfileClientId",
+                schema: "clientprofiles",
+                table: "blockers",
+                newName: "ProfileClientId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_blockers_ClientProfileClientId",
+                schema: "clientprofiles",
+                table: "blockers",
+                newName: "IX_blockers_ProfileClientId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_blockers_profiles_ProfileClientId",
+                schema: "clientprofiles",
+                table: "blockers",
+                column: "ProfileClientId",
+                principalSchema: "clientprofiles",
+                principalTable: "profiles",
+                principalColumn: "ClientId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_blockers_profiles_ProfileClientId",
+                schema: "clientprofiles",
+                table: "blockers");
+
+            migrationBuilder.RenameColumn(
+                name: "ProfileClientId",
+                schema: "clientprofiles",
+                table: "blockers",
+                newName: "ClientProfileClientId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_blockers_ProfileClientId",
+                schema: "clientprofiles",
+                table: "blockers",
+                newName: "IX_blockers_ClientProfileClientId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_blockers_profiles_ClientProfileClientId",
+                schema: "clientprofiles",
+                table: "blockers",
+                column: "ClientProfileClientId",
+                principalSchema: "clientprofiles",
+                principalTable: "profiles",
+                principalColumn: "ClientId");
+        }
+    }
+}
