c761073c48cab97e1f856a83dd365708e8c800d3
Pavlo Pylypenko <pavel.p@smplt.net>
2022-04-06 17:54:53 +0300

    migrations

---
+++ Service.ClientProfile.Postgres/Migrations/20220406142608_MarketingFlag.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Service.ClientProfile.Postgres.Migrations
+{
+    public partial class MarketingFlag : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "MarketingEmailAllowed",
+                schema: "clientprofiles",
+                table: "profiles",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MarketingEmailAllowed",
+                schema: "clientprofiles",
+                table: "profiles");
+        }
+    }
+}
