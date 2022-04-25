74e17770c2a905f908384f5649c15b2ad5a84230
Hays R <wonderhays@gmail.com>
2022-04-25 10:29:17 +0300

    bloker improvements

--- src/Service.ClientProfile/Services/ClientProfileService.cs
+++ src/Service.ClientProfile/Services/ClientProfileService.cs
@@ -42,7 +42,7 @@ namespace Service.ClientProfile.Services
 
             await foreach (var blocker in clientBlockers)
             {
-                blocker.ClientId = blocker.Profile.ClientId;
+                blocker.ClientId = blocker?.Profile?.ClientId;
                 yield return blocker;
             }
         }
